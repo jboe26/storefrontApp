ba65634a4a7c95022501d4f6b40a10f9ee1564ca
jboe26 <joshrboepple@outlook.com>
2019-10-02 17:13:54 -0400

    got the products to show up

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -25,4 +25,39 @@ connection.connect(function (err) {
 
 // function which prompts the user for what action they should take
 
+function start() {
+  // query the database for all items being auctioned
+  connection.query("SELECT * FROM products", function(err, results) {
+    if (err) throw err;
+    // once you have the items, prompt the user for which they'd like to bid on
+    inquirer
+      .prompt([
+        {
+          name: "choice",
+          type: "rawlist",
+          choices: function() {
+            var itemsArray = [];
+            for (var i = 0; i < results.length; i++) {
+              itemsArray.push(results[i].product_name);
+            }
+            return itemsArray;
+          },
+          message: "Welcome To BAMAZON!"
+        }
+      ])
+      .then(function(answer) {
+        // get the information of the chosen item
+        var chosenItem;
+        for (var i = 0; i < results.length; i++) {
+          if (results[i].item_name === answer.choice) {
+            chosenItem = results[i];
+          }
+        }
 
+        
+
+
+
+      });
+  });
+}
